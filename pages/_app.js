22cf5148b7f766aa9772b3c73f875a915dc74901
Vikram Sridhar <vikram@Vikrams-MacBook-Pro.local>
2022-03-04 04:03:30 -0500

    initial commit #2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { AnimateSharedLayout } from 'framer-motion'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AnimateSharedLayout>
+        <Component {...pageProps} />
+      </AnimateSharedLayout>
+
+    </>
+  )
 }
 
 export default MyApp
